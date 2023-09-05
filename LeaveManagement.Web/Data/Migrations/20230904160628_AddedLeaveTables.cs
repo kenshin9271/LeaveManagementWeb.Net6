324472ca581e3cb5f1c07a3e080f1358d0a828e5
charles wong <charleswong080191@gmail.com>
2023-09-05 23:41:33 +0800

    added Database Tables and Migrations. End Implement Database Section

---
+++ LeaveManagement.Web/Data/Migrations/20230904160628_AddedLeaveTables.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveManagement.Web.Data.Migrations
+{
+    public partial class AddedLeaveTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "LeaveTypes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "LeaveTypes");
+        }
+    }
+}
